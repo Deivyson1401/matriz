0198d23b9736d0c94f0272c35990d21808a949de
Deivyson J. Silva <139878479+Deivyson1401@users.noreply.github.com>
2024-10-12 00:22:04 +0000

    atv matrizes

---
+++ funcoes/criar_matriz.py
@@ -0,0 +1,12 @@
+from funcoes.codigo_caracteres import caracteres
+
+def criar_matriz(texto: str):
+    matriz: list
+
+    for x in range(10):
+
+        for y in range(10):
+            
+            print()
+
+    return matriz
